13758d656e1395bebd75f7950cbce57bb56b6070
kungza0123 <6331305002@lamduan.mfu.ac.th>
2022-11-18 20:56:50 +0700

    firstcommit

---
+++ src/main/java/com/mfu/ProjectEnterprise/ProjectEnterpriseApplication.java
@@ -0,0 +1,13 @@
+package com.mfu.ProjectEnterprise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectEnterpriseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectEnterpriseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mfu/ProjectEnterprise/ProjectEnterpriseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mfu.ProjectEnterprise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectEnterpriseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
